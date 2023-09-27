b76601abd5e033bc6ad34ac7b819dca5010cb22e
James Calver <james_calver@hotmail.co.uk>
2023-09-27 18:56:17 +0100

    Leetcode : 27/09/2023

---
+++ Two Pointers/ valid_palindrome.py
@@ -0,0 +1,49 @@
+'''
+https://leetcode.com/problems/valid-palindrome
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and 
+removing all non-alphanumeric characters, it reads the same forward and backward. 
+Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+'''
+
+# My Solution
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join([i for i in s if i.isalnum()]).lower()
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        
+        return True
+
+# Neetcode Solution
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphanum(s[l]):
+                l += 1
+            while l < r and not self.alphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+    # Could write own alpha-numeric function
+    def alphanum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
